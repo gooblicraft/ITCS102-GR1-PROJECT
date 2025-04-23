5cf4740c390b57cd11691f73faf21715f9e60364
Gooblicraft <148702245+gooblicraft@users.noreply.github.com>
2025-04-23 17:11:28 +0800

    update

--- jabi.py
+++ jabi.py
@@ -1,1 +1,11 @@
-helloworld("print")
+from customtkinter import *
+
+mainWindow = CTk()
+mainWindow.geometry("230x230")
+
+set_appearance_mode("light")
+set_default_color_theme("dark-blue")
+
+
+
+mainWindow.mainloop()
