a70ae9014c3cba7bed1b2db377cc16ed46e3637a
Warren Tomo <warrentomo0719@gmail.com>
2025-04-29 17:19:25 +0800

    omg yhes eyes yes

---
+++ GUIScanner.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+
+
+main = Tk()
+main.title("QR SCANNER NA BINASTA")
+main.geometry("960x540")
+main.resizable(False,False)
+
+
+bg_image = Image.open('tkinterrrrr\\tikintir\\qrscannibal.png')
+
+bg_photo = ImageTk.PhotoImage(bg_image)
+
+
+
+# --- Canvas for Background and Floating Text ---
+canvas = Canvas(main, width=639, height=639, highlightthickness=0, bd=0)
+canvas.pack(fill="both", expand=True)
+
+# Place Background Image on Canvas
+canvas.create_image(0, 0, anchor='nw', image=bg_photo)
+
+
+main.mainloop()
